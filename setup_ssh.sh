af66cb62afcb2c0e34ad9b9bfa516ce7eb8f96e9
konradko <konrad.korzel@gmail.com>
2016-08-08 22:08:10 +0100

    Add restoring ssh keys from first install

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -5,4 +5,14 @@ mkdir -p ~/.ssh
 # CLIENT_PUBKEY is set via resin.io env vars
 echo $CLIENT_PUBKEY | tee -a ~/.ssh/authorized_keys
 
+if [ -d "/data/ssh" ]; then
+    # Restore keys from first openssh-server install
+    cp /data/ssh/ssh_host_*_key* /etc/ssh/
+else
+    # Save keys from first openssh-server install
+    mkdir /data/ssh
+    cp /etc/ssh/ssh_host_*_key* /data/ssh/
+
+fi
+
 service ssh start
